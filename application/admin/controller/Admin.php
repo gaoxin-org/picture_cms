1851f6837f9fb7f9d5878b19197154e784c56d1e
sxgxy2008 <sxgxy2008@163.com>
2017-01-21 18:04:20 +0800

    bug

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -17,7 +17,7 @@ class Admin extends Common
 			if($data['password_n']!=$data['password_r']){
 				$this->error('新密码与重复密码不一致');
 				}
-			 $encrypt=create_randomstr();
+			 $encrypt=$this->create_randomstr();
 			 $password=md5(md5($data['password_n']).$encrypt);	
 			 db('admin')->where('id',$id)->update(['password'=>$password,'encrypt'=>$encrypt]);
 			 $this->success('密码修改成功','Login/index');	
@@ -25,5 +25,21 @@ class Admin extends Common
 	    return $this->fetch();
 	}
 		
+	/*
+	 * Create a random string
+	 * @author	XEWeb <>
+	 * @param $length the length of the string to create
+	 * @return $str the string
+	 */
+	function create_randomstr($length = 6) {
+		$str = "";
+		$characters = array_merge(range('A','Z'), range('a','z'), range('0','9'));
+		$max = count($characters) - 1;
+		for ($i = 0; $i < $length; $i++) {
+			$rand = mt_rand(0, $max);
+			$str .= $characters[$rand];
+		}
+		return $str;
+	}
    	
 }
